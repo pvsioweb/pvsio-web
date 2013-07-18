cc71c413ccbe1805c9e9dd3f64e9e498c88f0ac0
Paolo Masci <paolo.masci@eecs.qmul.ac.uk>
2013-07-18 15:02:01 +0100

    Update gip.js
    minor change to reflect the formatting of the state (no spaces between field name and := )

--- public/demos/GPCA-UI_PVS/NavKeys/scripts/gip.js
+++ public/demos/GPCA-UI_PVS/NavKeys/scripts/gip.js
@@ -1,14 +1,14 @@
 function GIPlog(msg){
 	var c = document.getElementById('GIP-Controller_console');
     label = "Controller state<br>";
-    msg = msg.substring(msg.indexOf("controller :=") + 13, msg.indexOf(", tick_enabled :="));
+    msg = msg.substring(msg.indexOf("controller:=") + 12, msg.indexOf(", tick_enabled:="));
 	c.innerHTML = "[ " + (new Date()).toTimeString() + " ]<br>" + label + msg + "<br><br>" + c.innerHTML;
 }
 
 function UIlog(msg){
 	var c = document.getElementById('GIP-UI_console');
     label = "User interface state<br>";
-    msg = msg.substring(msg.indexOf("ui :=") + 5, msg.lastIndexOf("#)"));
+    msg = msg.substring(msg.indexOf("ui:=") + 5, msg.lastIndexOf("#)"));
 	c.innerHTML = "[ " + (new Date()).toTimeString() + " ]<br>" + label + msg + "<br><br>" + c.innerHTML;
 }
 
