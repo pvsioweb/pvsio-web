9f9c09ead017b9257117ed05744cd1ee43c81a79
Paolo Masci <paolo.masci@eecs.qmul.ac.uk>
2013-11-25 14:16:05 +0000

    temporary fix for Display.js to show fractional numbers & strings
    TODO: remember to sort out Patrick's concerns on this :)

--- public/pvsioweb/app/plugins/prototypebuilder/Display.js
+++ public/pvsioweb/app/plugins/prototypebuilder/Display.js
@@ -26,8 +26,17 @@ define(function (require, exports, module) {
 			var regexp = new RegExp(this.regex());
 			var match = regexp.exec(stateString);
 			if (match && match.length > 1) {
-				///TODO remember to sort out Paolo's bugfix (@69992fda)
-				return match[1].toString();
+				///TODO: remember to sort out Patrick's concerns on this :) [was: remember to sort out Paolo's bugfix (@69992fda)]
+				// return match[1].toString();
+				if(regexp.toString().indexOf("[0-9/.]+") >= 0) {
+					// it's a number
+					match = eval(match[1].toString());
+				}
+				else {
+					// don't evalutate the expression, and remove double quotes if needed
+					match = match[1].toString().replace(new RegExp("\"","g"),"")
+				}
+				return match;
 			}
 		}
 		return;
