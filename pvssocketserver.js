b6ecda35f3115c14d9a580736f24d7e5cefa50d3
PaoloMasci <paolo.masci@eecs.qmul.ac.uk>
2014-05-07 14:23:16 -0700

    code cleanup

--- pvssocketserver.js
+++ pvssocketserver.js
@@ -483,18 +483,6 @@ function run() {
                     }
                     processCallback(res, socket);
                 });
-            },
-            "toPDF": function (token, socket, socketid) {
-                var PDFHandlerObj = new PDFHandler();
-                var path =  'public/' + 'tmp' + socketid + '/';
-                var pathDownload = 'tmp' + socketid + '/';
-                PDFHandlerObj.createPDF(token, path, function (exitStatus) {
-                    var res = {id: token.id};
-                    if (exitStatus < 0) {
-                        res.err = 'There was an issue creating PDF';
-                    } else { res.path = pathDownload + 'out.pdf'; }
-                    processCallback(res, socket);
-                });
             }
         };
 
