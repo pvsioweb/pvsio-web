39a3c0654d6e1a3b3a40ca25b09dc863f290284c
Paolo Masci <paolo.masci@gmail.com>
2014-09-11 11:59:49 +0100

    patch for bug #102 (image not refreshed) + updated blank form so that image start.png is not required anymore

--- src/client/app/project/ProjectManager.js
+++ src/client/app/project/ProjectManager.js
@@ -339,6 +339,11 @@ define(function (require, exports, module) {
                 .then(function (res) {
                     project.changeImage(project.name() + "/" + res.filePath, res.fileContent);
                 });
+        } else {
+            d3.select("#imageDiv img").attr("src", "").attr("height", "430").attr("width", "1128");
+            d3.select("#imageDiv svg").attr("height", "430").attr("width", "1128");
+            // show the draganddrop stuff
+            d3.select("#imageDragAndDrop.dndcontainer").style("display", "block");
         }
         
         //create promises for the pvs source files, if any is specified in data
