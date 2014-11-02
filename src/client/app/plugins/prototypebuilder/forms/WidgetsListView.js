3b980343874b88e70308b9fda29fbb3c947533b1
Paolo Masci <paolo.masci@gmail.com>
2014-11-02 18:27:35 +0000

    fix for issue #145 + minor code cleanup

--- src/client/app/plugins/prototypebuilder/forms/WidgetsListView.js
+++ src/client/app/plugins/prototypebuilder/forms/WidgetsListView.js
@@ -4,62 +4,64 @@
  * @date 9/17/14 14:40:29 PM
  */
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
-/*global define, require, Backbone, Handlebars, $, d3*/
+/*global define, require, Backbone, Handlebars, $, d3, Event*/
 define(function (require, exports, module) {
 	"use strict";
 	var WidgetManager = require("pvsioweb/WidgetManager").getWidgetManager();
 	var itemTemplate = require("text!./templates/widgetListItem.handlebars");
 
-	function WidgetsListView (widgets) {
+	function WidgetsListView(widgets) {
 		var el = d3.select("#widgetsList").html("").append("ul");
 		
 		function update(data) {
-			var listItems = el.selectAll("li.list-group-item").data(data);
-			listItems.enter().append("li").attr("class", "list-group-item")
-				.attr("widget-id", function (w) {
-					return w.id();
-				})
-				.on("click", function (w) {
-					var event = d3.event;
-					if (!event.shiftKey) {
-						d3.selectAll("g.selected").classed("selected", false);
-						listItems.classed("selected", false);
-					}
-					d3.select(this).classed("selected", true);
-					d3.select(w.parentGroup()).classed("selected", true);
-					event.preventDefault();
-					event.stopPropagation();
-				}).on("dblclick", function (w) {
-					var event = d3.event;
-					var dblclick = new Event("dblclick");
-					w.element().node().dispatchEvent(dblclick);
-					event.preventDefault();
-					event.stopPropagation();
-				});
-			//update
-			listItems.html(function (widget, i) {
-				var template = Handlebars.compile(itemTemplate);
-				return template(widget.toJSON());
-			});
-			//remove
-			listItems.exit().remove();
+            var listItems = el.selectAll("li.list-group-item").data(data, function (data) {
+                return data.id();
+            });
+            var enteredItems = listItems.enter();
+            var exitedItems = listItems.exit();
+            enteredItems.append("li").attr("class", "list-group-item")
+                .attr("widget-id", function (w) {
+                    d3.selectAll("#widgetsList ul li").classed("selected", false);
+                    return w.id();
+                }).classed("selected", true)
+                .text(function (widget, i) {
+                    var label = widget.type() + ": " + widget.displayKey();
+                    return label;
+                }).on("click", function (w) {
+                    var event = d3.event;
+                    if (!event.shiftKey) {
+                        d3.selectAll("g.selected").classed("selected", false);
+                        d3.selectAll("#widgetsList ul li").classed("selected", false);
+                    }
+                    d3.select(this).classed("selected", true);
+                    d3.select(w.parentGroup()).classed("selected", true);
+                    event.preventDefault();
+                    event.stopPropagation();
+                }).on("dblclick", function (w) {
+                    var event = d3.event;
+                    var dblclick = new Event("dblclick");
+                    w.element().node().dispatchEvent(dblclick);
+                    event.preventDefault();
+                    event.stopPropagation();
+                });
+            exitedItems.transition().duration(220).style("opacity", 0).remove();
 		}
 		
 		update(widgets);
 		
 		WidgetManager.addListener("WidgetModified", function (event) {
 			switch (event.action) {
-				case "create":
-					widgets.push(event.widget);
-					break;
-				case "remove":
-					var index = widgets.indexOf(event.widget);
-					if (index > -1) {
-						widgets.splice(index, 1);	
-					}
-					break;
-				default:
-					break;
+            case "create":
+                widgets.push(event.widget);
+                break;
+            case "remove":
+                var index = widgets.indexOf(event.widget);
+                if (index > -1) {
+                    widgets.splice(index, 1);
+                }
+                break;
+            default:
+                break;
 			}
 			update(widgets);
 		}).addListener("WidgetSelected", function (event) {
@@ -73,7 +75,7 @@ define(function (require, exports, module) {
 	module.exports = {
 		create: function () {
 			var widgets = WidgetManager.getAllWidgets();
-			return new WidgetsListView(widgets);	
+			return new WidgetsListView(widgets);
 		}
 	};
 });
