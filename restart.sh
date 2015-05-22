f98d2ca1bf9cbc0ec2f6f94ce27aa591831352ec
Paolo Masci <paolo.masci@gmail.com>
2015-05-22 23:02:52 +0100

    added installation script for glassfish

--- restart.sh
+++ restart.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 npm install
+if [ -d "src/server/lib" ] && [ -f "src/server/lib/glassfish-4-1.zip.partaa" ] && 
+	[ -f "src/server/lib/glassfish-4-1.zip.partab" ] && 
+	[ -f "src/server/lib/glassfish-4-1.zip.partac" ] && 
+	[ -f "src/server/lib/glassfish-4-1.zip.partad" ] && 
+	[ -f "src/server/lib/glassfish-4-1.zip.partae" ] && 
+	[ -f "src/server/lib/glassfish-4-1.zip.partaf" ] && 
+	[ -f "src/server/lib/glassfish-4-1.zip.partag" ] && 
+	[ ! -d "src/server/lib/glassfish4" ]; then
+  cd src/server/lib/
+  ./installNC.sh
+  cd ../../..
+fi
 cd src/server
 node pvssocketserver.js restart
