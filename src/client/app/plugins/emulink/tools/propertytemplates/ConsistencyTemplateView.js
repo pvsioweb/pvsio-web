64a593366334cccb747ae7077806f10d38cd6844
paolomasci <paolo.masci@gmail.com>
2017-02-03 12:31:30 +0000

    proof strategies for pvs theorems formulated using structural induction

--- src/client/app/plugins/emulink/tools/propertytemplates/ConsistencyTemplateView.js
+++ src/client/app/plugins/emulink/tools/propertytemplates/ConsistencyTemplateView.js
@@ -10,10 +10,20 @@
      var d3 = require("d3/d3"),
          formTemplate = require("text!./consistency/frontend.handlebars"),
          theoremTemplate = require("text!./consistency/consistency.handlebars"),
+         prooflite_strategy_induction  = require("text!./prooflite_strategy_induction.handlebars"),
+         pvs_strategy_induction  = require("text!./pvs_strategy_induction.handlebars"),
+         pvs_theorem_induction  = require("text!./pvs_theorem_induction.handlebars"),
          BaseDialog = require("pvsioweb/forms/BaseDialog"),
          FormUtils = require("plugins/emulink/forms/FormUtils");
 
      var default_data = { actions: ["act"], R: "R", s: "s" };
+     var theorem_data = {
+         theorem_name: "CONSISTENCY",
+         transition_relation: "trans",
+         property: "consistency",
+         state: "State",
+         guard: "guard"
+     };
      var ConsistencyTemplateView = BaseDialog.extend({
          initialize: function (data) {
              d3.select(this.el).attr("class", "overlay").style("top", self.scrollY + "px");
@@ -26,6 +36,12 @@
              this.$el.html(template(data));
              $("body").append(this.el);
              d3.select("#pvs_property").html(Handlebars.compile(theoremTemplate)(default_data));
+             d3.select("#pvs_theorem").html(Handlebars.compile(pvs_theorem_induction)(theorem_data));
+             d3.select("#prooflite_strategy").html(Handlebars.compile(prooflite_strategy_induction, { noEscape: true })({
+                 theorem: theorem_data.theorem_name,
+                 transition_relation: theorem_data.transition_relation,
+                 transition_names: default_data.actions
+             }));
              return this;
          },
          events: {
@@ -55,13 +71,18 @@
              if (attributes && attributes.selectedOptions && attributes.selectedOptions.length > 0) {
                  data.s = d3.select("#ConsistencyTemplate").select("#stateVariable").node().selectedOptions[0].value.replace(/\./g, "`") || data.s;
              }
-             //  d3.select("#pvs_property").html(Handlebars.compile(theoremTemplate)(data));
-             d3.select("#pvs_property").node().value = Handlebars.compile(theoremTemplate)(data);
+             d3.select("#pvs_property").node().value = Handlebars.compile(theoremTemplate, { noEscape: true })(data);
+             d3.select("#pvs_theorem").node().value = Handlebars.compile(pvs_theorem_induction, { noEscape: true })(theorem_data);
+             d3.select("#prooflite_strategy").node().value = Handlebars.compile(prooflite_strategy_induction, { noEscape: true })({
+                 theorem: theorem_data.theorem_name,
+                 transition_relation: theorem_data.transition_relation,
+                 transition_names: data.actions
+             });
          },
          right: function (event) {
              var form = this.el;
              if (FormUtils.validateForm(form)) {
-                 var selectors = [ "pvs_property" ];
+                 var selectors = [ "pvs_property", "pvs_theorem", "prooflite_strategy" ];
                  var formdata = FormUtils.serializeForm(form, selectors);
                  this.trigger(this._data.buttons[1].toLowerCase(), {data: formdata.labels, el: this.el}, this);
              }
