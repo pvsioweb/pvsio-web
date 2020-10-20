63d246cede36688011592626560020de4cdd490a
Paolo Masci <paolo.masci@gmail.com>
2020-10-19 23:11:35 -0400

    Update pvssocketserver.js
    removed folder constraints for localhost

--- src/server/pvssocketserver.js
+++ src/server/pvssocketserver.js
@@ -675,9 +675,6 @@ function run() {
                 if (isAbsolute(token.path)) {
                     // remove all ../ to avoid writing in arbitrary parts of the file system
                     token.path = token.path.replace(/\.\./g, "");
-		    if (!token.path.startsWith(baseExamplesDir)) {
-                        token.path = path.join(baseExamplesDir, token.path);
-                    }
                 } else {
                     token.path = path.join(baseProjectDir, token.path);
                 }
