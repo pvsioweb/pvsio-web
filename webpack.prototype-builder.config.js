8a0abee0ca1035cacb78edcd2f5c2c69410f6f3a
Paolo M. Masci <paolo.m.masci@nasa.gov>
2021-02-10 17:51:45 -0500

    disabled webpack name mangling

--- webpack.prototype-builder.config.js
+++ webpack.prototype-builder.config.js
@@ -11,6 +11,9 @@ module.exports = {
     resolve: {
         extensions: ['.tsx', '.ts', '.js']
     },
+    optimization: {
+        minimize: false
+    },
     module: {
         rules: [
             {
